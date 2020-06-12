a25d9880c0aba1c5af83820e039ae1ff0e53a1a5
Aan Budi Setiawan <setiawan.aanbudi@gmail.com>
2020-06-12 22:06:44 +0700

    add producer

--- voucher-service/src/main/java/com/danapprentech/debrief2/voucherservice/rabbit/consumer/Consumer.java
+++ voucher-service/src/main/java/com/danapprentech/debrief2/voucherservice/rabbit/consumer/Consumer.java
@@ -1,21 +1,26 @@
 package com.danapprentech.debrief2.voucherservice.rabbit.consumer;//package com.danapprentech.debrief2.voucherservice.rabbit.consumer;
 
 import com.danapprentech.debrief2.voucherservice.model.Voucher;
+import com.danapprentech.debrief2.voucherservice.model.response.VoucherResponse;
 import com.danapprentech.debrief2.voucherservice.rabbit.model.Transaction;
 import com.danapprentech.debrief2.voucherservice.rabbit.model.UpdateQtyConsumer;
+import com.danapprentech.debrief2.voucherservice.rabbit.producer.RabbitMqProducer;
 import com.danapprentech.debrief2.voucherservice.repository.VoucherRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.*;
 
 @Component
 public class Consumer
 {
     @Autowired
     VoucherRepository voucherRepository;
+
+    @Autowired
+    RabbitMqProducer mqProducer;
 
 //    @RabbitListener(queues = "${spring.rabbitmq.queue.listener}",containerFactory = "createListener")
 //    public void updateQuota(Transaction updateQtyConsumer)
@@ -43,5 +48,21 @@ public class Consumer
         System.out.println("update sukses");
         System.out.println(vouchers.getQuota());
         voucherRepository.save(vouchers);
+
+        // kirim keyogi
+
+        Voucher voucher = voucherRepository.findByVoucherName(vouchers.getVoucherName());
+        VoucherResponse voucherResponse = new VoucherResponse();
+        voucherResponse.setVoucherName(voucher.getVoucherName());
+        voucherResponse.setDiscount(voucher.getDiscount());
+        voucherResponse.setVoucherPrice(voucher.getVoucherPrice());
+        voucherResponse.setMaxDiscount(voucher.getMaxDiscount());
+        voucherResponse.setQuota(voucher.getQuota());
+        voucherResponse.setExpiredDate(voucher.getExpiredDate());
+        voucherResponse.setStatus(voucher.getStatus());
+        voucherResponse.setIdMerchant(voucher.getMerchant().getIdMerchant());
+        voucherResponse.setIdVoucher(voucher.getIdVoucher());
+        voucherResponse.setMerchantName(voucher.getMerchant().getMerchantName());
+        mqProducer.sendToRabbitVoucher(voucherResponse);
     }
 }
