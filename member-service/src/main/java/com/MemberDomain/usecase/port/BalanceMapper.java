b72211ca79c7f893d4b045edbe81db614fdbabf0
aldieadrian <aldieadrianfillmore@gmail.com>
2020-06-12 21:35:42 +0700

    Transaction Listener

--- member-service/src/main/java/com/MemberDomain/usecase/port/BalanceMapper.java
+++ member-service/src/main/java/com/MemberDomain/usecase/port/BalanceMapper.java
@@ -10,7 +10,12 @@ public interface BalanceMapper {
     final String registerBalance = "INSERT INTO tbl_balances (idUser, balance)\n" +
             "VALUES (#{idUser}, '0');";
 
+    final String updateBalance = "UPDATE tbl_users SET balance = #{balance} WHERE idUser = #{idUser}";
+
     @Insert(registerBalance)
     void registerBalance(String idUser);
+
+    @Update(updateBalance)
+    void updateBalance(double finalAmount, String idUser);
 
 }
