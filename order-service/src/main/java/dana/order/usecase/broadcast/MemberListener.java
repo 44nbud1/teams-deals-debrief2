05d16bf73d36b772c818e7d8645039ff8d91dc30
achmadyogi <achmad.yogi.prakoso@gmail.com>
2020-06-12 21:33:27 +0700

    About Member Synchronization

--- order-service/src/main/java/dana/order/usecase/broadcast/MemberListener.java
+++ order-service/src/main/java/dana/order/usecase/broadcast/MemberListener.java
@@ -2,6 +2,7 @@ package dana.order.usecase.broadcast;
 
 import dana.order.entity.User;
 import dana.order.usecase.port.DatabaseMapper;
+import dana.order.usecase.port.UserRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,10 +14,17 @@ public class MemberListener {
     @Autowired
     DatabaseMapper databaseMapper;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Qualifier("fanoutMember")
     @RabbitListener(queues = "#{autoDeleteQueue.name}")
     public void receive(User user) {
-        databaseMapper.createNewUser(user);
+        if (userRepository.doesUserExist(user.getIdUser()) == Boolean.FALSE){
+            databaseMapper.createNewUser(user);
+        }else{
+            databaseMapper.updateAUser(user);
+        }
         System.out.println("A new user: "+user.toJsonString());
     }
 }
