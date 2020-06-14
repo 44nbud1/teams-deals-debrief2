f2218970c281c558907e9e00f9288593d2762042
achmadyogi <achmad.yogi.prakoso@gmail.com>
2020-06-14 13:28:57 +0700

    Set Queue Local

--- order-service/src/main/java/dana/order/usecase/exception/HistoryFailedException.java
+++ order-service/src/main/java/dana/order/usecase/exception/HistoryFailedException.java
@@ -6,18 +6,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class HistoryFailedException extends ResponseStatusException {
 
-    private String internalStatusCode;
-
     public HistoryFailedException(DealsStatus status){
         super(status.getStatus(), status.getMessage());
-        this.internalStatusCode = status.getValue();
     }
 
-    public String getInternalStatusCode() {
-        return internalStatusCode;
-    }
-
-    public void setInternalStatusCode(String internalStatusCode) {
-        this.internalStatusCode = internalStatusCode;
-    }
 }
